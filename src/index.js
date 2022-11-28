26564607db1bf6525ee1c1071eae781d7bc2a360
mexxiik <118180391+mexxiik@users.noreply.github.com>
2022-11-28 14:08:55 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./styles.css"; // keep this here!
-
 // naimportujte vše co je potřeba z BabylonJS
 import {
   Engine,
@@ -19,33 +18,25 @@ import {
   Animation
 } from "@babylonjs/core";
 import "@babylonjs/inspector";
-
 //canvas je grafické okno, to rozáhneme přes obrazovku
 const canvas = document.getElementById("renderCanvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const engine = new Engine(canvas, true);
-
 //scéna neměnit
 const scene = new Scene(engine);
 // Default Environment
-
 //vytoření kamery v pozici -5 (dozadu)
 const camera = new DeviceOrientationCamera(
   "kamera",
   new Vector3(1, 1, 10),
   scene
 );
-
 //zaměřit kameru do středu
 camera.setTarget(new Vector3(0, 1, 0));
-
 //spojení kamery a grafikcého okna
 camera.attachControl(canvas, true);
-
 //zde přídáme cyklus for
-
 //světlo
 const light1 = new DirectionalLight(
   "DirectionalLight",
@@ -54,12 +45,50 @@ const light1 = new DirectionalLight(
 );
 
 //vytvoření cesty
+var points = [];
+var n = 450; //pocet bodu
+var r = 50; //radius krivky
+for (var i = 0; i < n + 1; i++) {
+  points.push(
+    new Vector3(
+      (r + (r / 3) * Math.sin((2 * i * Math.PI) / n)) *
+        Math.sin((2 * i * Math.PI) / n),
+      0,
+      (r + (r / 1) * Math.sin((2 * i * Math.PI) / n)) *
+        Math.cos((2 * i * Math.PI) / n)
+    )
+  );
+}
 
 //vykreslení křivky
+var track = MeshBuilder.CreateLines("track", { points: points });
+var freza = MeshBuilder.CreateCylinder("freza", { diameter: 0.00001 });
+SceneLoader.ImportMesh("", "public/", "endmill.glb", scene, function (
+  noveModely
+) {
+  freza = noveModely[0];
+  freza.scaling = new Vector3(0.15, 0.15, 0.25);
+  freza.position.y = 6;
+  freza.rotate(new Vector3(1, 0, 0), Math.PI / 2);
+});
 
 //úhly a rotace
+var path3d = new Path3D(points);
+var normals = path3d.getNormals();
+var theta = Math.acos(Vector3.Dot(Axis.Z, normals[0]));
+freza.rotate(Axis.X, theta + 5, Space.WORLD);
 
 //animace
+var i = 0;
+scene.registerAfterRender(function () {
+  freza.position.x = points[i].x;
+  freza.position.z = points[i].z;
+  theta = Math.acos(Vector3.Dot(normals[0], normals[i + 1]));
+  var sklopeni = Vector3.Cross(normals[i], normals[i + 1]).y;
+  sklopeni = sklopeni / Math.abs(sklopeni);
+  freza.rotate(Axis.Y, sklopeni * theta, Space.WORLD);
+  i = (i + 1) % (n - 1);
+});
 
 // povinné vykreslování
 engine.runRenderLoop(function () {
@@ -69,5 +98,4 @@ const environment1 = scene.createDefaultEnvironment({
   enableGroundShadow: true
 });
 // zde uděláme VR prostředí
-
 //scene.debugLayer.show();
